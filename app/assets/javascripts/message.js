258cd13132ce28e9cc44ebd11c8cf50ff4d9a560
aoyamayumeto <60101024+aoyamayumeto@users.noreply.github.com>
2020-03-02 14:59:22 +0900

    非同期通信実装（レビュー後①）

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -43,6 +43,7 @@ $('#new_message').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
  var url = $(this).attr('action')
+ 
  $.ajax({
    url: url,
    type: "POST",
@@ -51,21 +52,17 @@ $('#new_message').on('submit', function(e){
    processData: false,
    contentType: false
  })
+
   .done(function(data){
     var html = buildHTML(data);
     $('.main-chat__message-list').append(html);      
     $('form')[0].reset();
     $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
     $('.submit-btn').prop("disabled", false);
-
   });
 
   .fail(function() {
     alert("メッセージ送信に失敗しました");
-  
-
 })
-
-
 
 });
